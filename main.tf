bf68edaad5fb0b81ed2cc2c35e8b2d0469981daf
mykytaoleksin <nacenik@gmail.com>
2021-11-16 12:28:05 +0200

    fixed

--- main.tf
+++ main.tf
@@ -53,11 +53,11 @@ resource "aws_security_group" "security_for_my_server" {
   }
 
   egress {
-    description = "http"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
