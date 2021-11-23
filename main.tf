a92cf063245b5930e57c614d67e380ed30d53500
mykytaoleksin <nacenik@gmail.com>
2021-11-23 12:47:40 +0200

    fixed

--- main.tf
+++ main.tf
@@ -69,15 +69,17 @@ resource "aws_instance" "my_linux" {
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y docker
+sudo service docker start
+sudo docker pull nacenik/stage-payment-system-aws:v2
+sudo docker run -d -p 8080:8080 nacenik/stage-payment-system-aws:v2
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 aws configure
 AKIAVQJWDT3DGEIBN3OI
 4ydcTsShQnKn4jQfni0q0229MDsTa6VenR1eNA3p
-sudo service docker start
-sudo docker pull nacenik/stage-payment-system-aws:v2
-sudo docker run -d -p 8080:8080 nacenik/stage-payment-system-aws:v2
+us-east-1
+
 
 
   EOT
