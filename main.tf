4bc89212639401790b9cf9ebe1c43a55aa30c4c3
mykytaoleksin <nacenik@gmail.com>
2021-11-24 14:00:54 +0200

    added iam role for ec2

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "ec2-read-only-policy-attachment" {
   role       = aws_iam_role.system_manager_role_for_ec2.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2RoleforSSM"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
