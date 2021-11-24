af0f38fd6800b892129219dc39281d41ee3038b6
mykytaoleksin <nacenik@gmail.com>
2021-11-24 13:55:24 +0200

    added iam role for ec2

--- main.tf
+++ main.tf
@@ -110,105 +110,30 @@ resource "aws_iam_role" "system_manager_role_for_ec2" {
   name = "system_manager_role_for_ec2"
 
   assume_role_policy = <<EOF
-  {
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ssm:DescribeAssociation",
-          "ssm:GetDeployablePatchSnapshotForInstance",
-          "ssm:GetDocument",
-          "ssm:DescribeDocument",
-          "ssm:GetManifest",
-          "ssm:GetParameters",
-          "ssm:ListAssociations",
-          "ssm:ListInstanceAssociations",
-          "ssm:PutInventory",
-          "ssm:PutComplianceItems",
-          "ssm:PutConfigurePackageResult",
-          "ssm:UpdateAssociationStatus",
-          "ssm:UpdateInstanceAssociationStatus",
-          "ssm:UpdateInstanceInformation"
-        ],
-        "Resource" : "*"
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "ec2.amazonaws.com"
+        ]
       },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ssmmessages:CreateControlChannel",
-          "ssmmessages:CreateDataChannel",
-          "ssmmessages:OpenControlChannel",
-          "ssmmessages:OpenDataChannel"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ec2messages:AcknowledgeMessage",
-          "ec2messages:DeleteMessage",
-          "ec2messages:FailMessage",
-          "ec2messages:GetEndpoint",
-          "ec2messages:GetMessages",
-          "ec2messages:SendReply"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "cloudwatch:PutMetricData"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ec2:DescribeInstanceStatus"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ds:CreateComputer",
-          "ds:DescribeDirectories"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:PutLogEvents"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:GetBucketLocation",
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:GetEncryptionConfiguration",
-          "s3:AbortMultipartUpload",
-          "s3:ListMultipartUploadParts",
-          "s3:ListBucket",
-          "s3:ListBucketMultipartUploads"
-        ],
-        "Resource" : "*"
-      }
-    ]
-  }
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
 EOF
-
   tags = {
     tag-key = "tag-value"
   }
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-read-only-policy-attachment" {
+  role       = aws_iam_role.system_manager_role_for_ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2RoleforSSM"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
